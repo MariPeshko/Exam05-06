730ee795eadf2ecc302a2bb08f4cad5d7506eadf
MariPeshko <marunapeshko@gmail.com>
2026-04-14 07:47:57 +0200

    message

--- Rank_06/solution_poll.c
+++ Rank_06/solution_poll.c
@@ -5,6 +5,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <poll.h>
+#include <stdlib.h> // atoi, calloc, realloc
+
+#include <stdio.h> // for test
+
+// --- Глобальні змінні для стану сервера ---
+struct pollfd	*pfds = NULL;
+char			**client_bufs = NULL; // Буфери для повідомлень кожного клієнта
+int				sockfd = -1;
+int				fd_count = 0;
+int				fd_size = 0;
 
 int extract_message(char **buf, char **msg) {
 	char	*newbuf;
@@ -78,51 +88,122 @@ int	get_listener_socket(int port)
 	return sockfd;
 }
 
-void	handle_client_data() {
-	// resend it to all other clients with 
-	// "client %d: " prefixed to every line.
-	// OR
-	// "server: client %d just left\n".
+void	handle_client_data(int sockfd, int pfd_i, 
+	int *fd_count, struct pollfd **pfds) {
+
+	char recv_buf[200000]; // Буфер для одного виклику recv
+	//int	sender_fd = pfds[pfd_i].fd;
+
+	size_t len = sizeof(recv_buf);
+	// read a message
+	// (int sockfd, void *buf, size_t len, int flags);
+	int nbytes = recv((*pfds)[pfd_i].fd, recv_buf, len, 0);
+	if (nbytes <= 0) {
+		if (nbytes == 0)
+			printf("server: client %d just left\n", (*pfds)[pfd_i].fd);
+		else
+			err ("Fatal error");
+		char msg_buf[100];
+        sprintf(msg_buf, "server: client %d just left\n", sender_fd);
+		del_from_pfds(pfds);
+		// send message
+		// reexamine the slot we just deleted
+		(pfd_i)--;
+	} else {
+		int sender_fd = (*pfds)[pfd_i].fd;
+
+		// 1. Додаємо отримані дані до персонального буфера клієнта
+        recv_buf[nbytes] = '\0';
+        client_bufs[pfd_i] = str_join(client_bufs[pfd_i], recv_buf);
+
+		// 2. Обробляємо всі повні повідомлення в буфері
+        char *msg;
+        while (extract_message(&client_bufs[pfd_i], &msg)) {
+            // 3. Формуємо фінальне повідомлення з префіксом
+            char broadcast_buf[4200]; // Розмір має бути достатнім
+            sprintf(broadcast_buf, "client %d: %s", sender_fd, msg);
+
+			for (int i = 0; i < *fd_count; i++) {
+				if ((*pfds)[i].fd != sender_fd || (*pfds)[i].fd != sockfd) {
+					if (send((*pfds)[i].fd, broadcast_buf, strlen(broadcast_buf), 0) == -1) err ("Fatal error");
+				}
+			}
+			// 5. Звільняємо пам'ять, виділену extract_message
+            free(msg);
+		}
+	}
+	
+	// if ((*pfds)[*pfd_i].revents & POLLIN) {
+
+	// } else {                /* POLLERR | POLLHUP */
+	// 	printf("server: client %d just left\n", (*pfds)[*pfd_i].fd);
+	// 	// del_from_pfds(pfds), 
+	// }
 }
 
-void	del_from_pfds() {}
+void	del_from_pfds(int pfd_i) {
+	(void)pfds;
+	close(pfds[pfd_i].fd);
+	free(client_bufs[pfd_i]);
+    // Зсуваємо останнього клієнта на місце видаленого
+    // Це швидше, ніж зсувати весь масив
+    if (pfd_i < fd_count - 1) {
+        pfds[pfd_i] = pfds[fd_count - 1];
+        client_bufs[pfd_i] = client_bufs[fd_count - 1];
+    }
+    fd_count--;
+}
 
-void	add_to_pfds(int *fd_count, int *fd_size, struct pollfd **pfds) {
+void	add_to_pfds(int connfd, int *fd_count, int *fd_size, struct pollfd **pfds) {
 	
-	*fd_count++;
+	if (*fd_count == *fd_size) {
+		*fd_size *= 2; // Double it
+		*pfds = realloc(*pfds, sizeof(**pfds) * (*fd_size));
+		// client_bufs
+		if (!*pfds) err ("Fatal error");
+	}
 
-	int indx = *fd_count - 1;
-	struct pollfd	pfds;
-	pfds[indx].fd = sockfd;
-	pfds[indx].events = POLLIN;
-	// if (*fd_count > *fd_size)
-	// realloc 
+	int indx = *fd_count;
+	(*pfds)[indx].fd = connfd;
+	(*pfds)[indx].events = POLLIN;
+	// revents ?
+
+	(*fd_count)++; // count connections;
 }
 
 void	handle_new_conn(
 	int sockfd, int *fd_count, int *fd_size, struct pollfd **pfds) {
 
 	struct sockaddr_in	cli;
-	len = sizeof(cli);
-	int	connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-	if (conndfd == -1) err("Fatal error");
-	add_to_pfds(fd_count, fd_size, pollfd);
+	socklen_t			len = sizeof(cli);
+	int					connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+	if (connfd == -1) err("Fatal error");
+	add_to_pfds(connfd, fd_count, fd_size, pfds);
+
+	char msg_buf[210000];
+	sprintf(msg_buf, "server: client %d just arrived\n", connfd);
+
+	for (int i = 0; i < *fd_count; i++) {
+		if ((*pfds)[i].fd != sockfd) {
+			if (send((*pfds)[i].fd, msg_buf, strlen(msg_buf), 0) == -1) err ("Fatal error");
+		}
+	}
 }
 
 void	process_connections(
 	int sockfd, int *fd_count, int *fd_size, struct pollfd **pfds) {
 		for (int i = 0; i < *fd_count; i++) {
-			if (*pfd[i].revents != 0) {
-				printf("  fd=%d; events: %s%s%s\n", pfds[j].fd,
-                    (*pfds[j].revents & POLLIN)  ? "POLLIN "  : "",
-                    (*pfds[j].revents & POLLHUP) ? "POLLHUP " : "",
-                    (*pfds[j].revents & POLLERR) ? "POLLERR " : "");
+			if ((*pfds)[i].revents != 0) {
+				printf("  fd=%d; events: %s%s%s\n", (*pfds)[i].fd,
+                    ((*pfds)[i].revents & POLLIN)  ? "POLLIN "  : "",
+                    ((*pfds)[i].revents & POLLHUP) ? "POLLHUP " : "",
+                    ((*pfds)[i].revents & POLLERR) ? "POLLERR " : "");
 			}
-			if (*pfd[i].fd == sockfd) {
+			if ((*pfds)[i].fd == sockfd) {
 				handle_new_conn(sockfd, fd_count, fd_size, pfds);
 				// to all: "server: client %d just arrived\n"
 			} else {
-				handle_client_data();
+				handle_client_data(sockfd, i, fd_count, pfds);
 			}
 		}
 }
@@ -152,6 +233,8 @@ int	main(int argc, char **argv) {
 
 		process_connections(sockfd, &fd_count, &fd_size, &pfds);
 	}
-
+	close(sockfd);
 	free (pfds);
+	free(client_bufs);
+    return 0;
 }
