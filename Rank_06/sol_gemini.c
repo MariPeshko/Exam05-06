3fb0a3293cc6da566443f9f7098f48acd361bab8
MariPeshko <marunapeshko@gmail.com>
2026-04-15 18:24:11 +0200

    mini_serv v.1 done

--- Rank_06/sol_gemini.c
+++ Rank_06/sol_gemini.c
@@ -8,14 +8,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// --- Глобальні змінні для стану сервера ---
+int client_ids[1024];
+int next_id = 0;
+
 struct pollfd	*pfds = NULL;
 char			**client_bufs = NULL; // Буфери для повідомлень кожного клієнта
 int				sockfd = -1;
 int				fd_count = 0;
 int				fd_size = 0;
-
-// --- Допоміжні функції (ваші extract_message та str_join залишаються без змін) ---
 
 int extract_message(char **buf, char **msg) {
     char	*newbuf;
@@ -62,9 +62,7 @@ void	err(char *msg) {
     exit(1);
 }
 
-// --- Основні функції сервера ---
-
-void	broadcast_message(int sender_fd, const char *msg) {
+void	broadcast_msg(int sender_fd, const char *msg) {
     for (int i = 0; i < fd_count; i++) {
         int dest_fd = pfds[i].fd;
         // Розсилаємо всім, крім слухаючого сокета та самого відправника
@@ -76,6 +74,18 @@ void	broadcast_message(int sender_fd, const char *msg) {
             }
         }
     }
+}
+
+void	remove_client(int pfd_i) {
+    close(pfds[pfd_i].fd);
+    free(client_bufs[pfd_i]);
+    // Зсуваємо останнього клієнта на місце видаленого
+    // Це швидше, ніж зсувати весь масив
+    if (pfd_i < fd_count - 1) {
+        pfds[pfd_i] = pfds[fd_count - 1];
+        client_bufs[pfd_i] = client_bufs[fd_count - 1];
+    }
+    fd_count--;
 }
 
 void	add_client(int connfd) {
@@ -91,45 +101,17 @@ void	add_client(int connfd) {
     fd_count++;
 }
 
-void	remove_client(int pfd_i) {
-    // Закриваємо сокет і звільняємо буфер
-    close(pfds[pfd_i].fd);
-    free(client_bufs[pfd_i]);
-
-    // Зсуваємо останнього клієнта на місце видаленого
-    // Це швидше, ніж зсувати весь масив
-    if (pfd_i < fd_count - 1) {
-        pfds[pfd_i] = pfds[fd_count - 1];
-        client_bufs[pfd_i] = client_bufs[fd_count - 1];
-    }
-    fd_count--;
-}
-
-void	handle_new_conn() {
-    struct sockaddr_in	cli;
-    socklen_t			len = sizeof(cli);
-    int					connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-    if (connfd == -1) return; // Не фатальна помилка, просто ігноруємо
-
-    add_client(connfd);
-
-    char msg_buf[100];
-    sprintf(msg_buf, "server: client %d just arrived\n", connfd);
-    broadcast_message(connfd, msg_buf); // Розсилаємо всім, крім нового клієнта
-}
-
 void	handle_client_data(int pfd_i) {
     char	recv_buf[4096]; // Буфер для одного виклику recv
     int		sender_fd = pfds[pfd_i].fd;
 
     int nbytes = recv(sender_fd, recv_buf, sizeof(recv_buf), 0);
 
-    if (nbytes <= 0) {
-        // Клієнт від'єднався або сталася помилка
+    if (nbytes <= 0) { // Клієнт від'єднався або сталася помилка
         char msg_buf[100];
         sprintf(msg_buf, "server: client %d just left\n", sender_fd);
         remove_client(pfd_i);
-        broadcast_message(sender_fd, msg_buf); // Повідомляємо всіх, що клієнт пішов
+        broadcast_msg(sender_fd, msg_buf); // Повідомляємо всіх, що клієнт пішов
     } else {
         // 1. Додаємо отримані дані до персонального буфера клієнта
         recv_buf[nbytes] = '\0';
@@ -143,7 +125,7 @@ void	handle_client_data(int pfd_i) {
             sprintf(broadcast_buf, "client %d: %s", sender_fd, msg);
 
             // 4. Розсилаємо його всім іншим
-            broadcast_message(sender_fd, broadcast_buf);
+            broadcast_msg(sender_fd, broadcast_buf);
 
             // 5. Звільняємо пам'ять, виділену extract_message
             free(msg);
@@ -151,13 +133,25 @@ void	handle_client_data(int pfd_i) {
     }
 }
 
+void	handle_new_conn() {
+	struct sockaddr_in	cli;
+	socklen_t			len = sizeof(cli);
+	int					connfd;
+    connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+	if (connfd == -1) err("Fatal error"); // TO DO: ? return; Не фатальна помилка, просто ігноруємо
+	
+	add_client(connfd);
+    
+	char msg_buf[100];
+	sprintf(msg_buf, "server: client %d just arrived\n", connfd);
+	broadcast_msg(connfd, msg_buf);
+}
+
 int	main(int argc, char **argv) {
     if (argc != 2) err("Wrong number of arguments");
 
     int port = atoi(argv[1]);
-    struct sockaddr_in servaddr;
-
-    // Створення та налаштування слухаючого сокета
+    struct sockaddr_in  servaddr;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) err("Fatal error");
 
@@ -170,23 +164,18 @@ int	main(int argc, char **argv) {
         err("Fatal error");
     if (listen(sockfd, 10) != 0) err("Fatal error");
 
-    // Додаємо слухаючий сокет до нашого набору
     add_client(sockfd);
-
-    // --- Головний цикл сервера ---
+    printf("Server is listening...\n");
     while(1) {
         if (poll(pfds, fd_count, -1) == -1) err("Fatal error on poll");
 
         // Перевіряємо події на всіх сокетах
         for (int i = 0; i < fd_count; i++) {
             if (pfds[i].revents & POLLIN) {
-                if (pfds[i].fd == sockfd) {
-                    // Нове з'єднання
+                if (pfds[i].fd == sockfd)
                     handle_new_conn();
-                } else {
-                    // Дані від існуючого клієнта
+                else
                     handle_client_data(i);
-                }
             }
         }
     }
--- Rank_06/solution_poll.c
+++ Rank_06/solution_poll.c
@@ -6,10 +6,11 @@
 #include <netinet/in.h>
 #include <poll.h>
 #include <stdlib.h> // atoi, calloc, realloc
-
 #include <stdio.h> // for test
 
-// --- Глобальні змінні для стану сервера ---
+int client_ids[1024];
+int next_id = 0;
+
 struct pollfd	*pfds = NULL;
 char			**client_bufs = NULL; // Буфери для повідомлень кожного клієнта
 int				sockfd = -1;
@@ -61,9 +62,19 @@ void	err(char *msg) {
 	exit(1);
 }
 
+void	broadcast_msg(int sender_fd, const char *msg) {
+    for (int i = 0; i < fd_count; i++) {
+        int dest_fd = pfds[i].fd;
+        if (dest_fd != sockfd && dest_fd != sender_fd) {
+            if (send(dest_fd, msg, strlen(msg), 0) == -1) {
+                printf("broadcast_msg: send err\n");
+            }
+        }
+    }
+}
+
 int	get_listener_socket(int port)
 {
-	int					sockfd;
 	struct	sockaddr_in	servaddr;
 
 	bzero(&servaddr, sizeof(servaddr));
@@ -76,75 +87,19 @@ int	get_listener_socket(int port)
 	if (sockfd == -1) err("Fatal error");
 	else
 		printf("Socket successfully created..\n");
-	
-	// Binding newly created socket to given IP and verification 
 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
 		err("Fatal error");
 	else
 		printf("Socket successfully bound.\n");
-	
 	if (listen(sockfd, 10) != 0) err("Fatal error");
-
 	return sockfd;
 }
 
-void	handle_client_data(int sockfd, int pfd_i, 
-	int *fd_count, struct pollfd **pfds) {
-
-	char recv_buf[200000]; // Буфер для одного виклику recv
-	//int	sender_fd = pfds[pfd_i].fd;
-
-	size_t len = sizeof(recv_buf);
-	// read a message
-	// (int sockfd, void *buf, size_t len, int flags);
-	int nbytes = recv((*pfds)[pfd_i].fd, recv_buf, len, 0);
-	if (nbytes <= 0) {
-		if (nbytes == 0)
-			printf("server: client %d just left\n", (*pfds)[pfd_i].fd);
-		else
-			err ("Fatal error");
-		char msg_buf[100];
-        sprintf(msg_buf, "server: client %d just left\n", sender_fd);
-		del_from_pfds(pfds);
-		// send message
-		// reexamine the slot we just deleted
-		(pfd_i)--;
-	} else {
-		int sender_fd = (*pfds)[pfd_i].fd;
-
-		// 1. Додаємо отримані дані до персонального буфера клієнта
-        recv_buf[nbytes] = '\0';
-        client_bufs[pfd_i] = str_join(client_bufs[pfd_i], recv_buf);
-
-		// 2. Обробляємо всі повні повідомлення в буфері
-        char *msg;
-        while (extract_message(&client_bufs[pfd_i], &msg)) {
-            // 3. Формуємо фінальне повідомлення з префіксом
-            char broadcast_buf[4200]; // Розмір має бути достатнім
-            sprintf(broadcast_buf, "client %d: %s", sender_fd, msg);
-
-			for (int i = 0; i < *fd_count; i++) {
-				if ((*pfds)[i].fd != sender_fd || (*pfds)[i].fd != sockfd) {
-					if (send((*pfds)[i].fd, broadcast_buf, strlen(broadcast_buf), 0) == -1) err ("Fatal error");
-				}
-			}
-			// 5. Звільняємо пам'ять, виділену extract_message
-            free(msg);
-		}
-	}
-	
-	// if ((*pfds)[*pfd_i].revents & POLLIN) {
-
-	// } else {                /* POLLERR | POLLHUP */
-	// 	printf("server: client %d just left\n", (*pfds)[*pfd_i].fd);
-	// 	// del_from_pfds(pfds), 
-	// }
-}
-
-void	del_from_pfds(int pfd_i) {
-	(void)pfds;
+// pfd_i - index of teh client in pfds array
+void	remove_client(int pfd_i) {
 	close(pfds[pfd_i].fd);
 	free(client_bufs[pfd_i]);
+
     // Зсуваємо останнього клієнта на місце видаленого
     // Це швидше, ніж зсувати весь масив
     if (pfd_i < fd_count - 1) {
@@ -154,87 +109,100 @@ void	del_from_pfds(int pfd_i) {
     fd_count--;
 }
 
-void	add_to_pfds(int connfd, int *fd_count, int *fd_size, struct pollfd **pfds) {
-	
-	if (*fd_count == *fd_size) {
-		*fd_size *= 2; // Double it
-		*pfds = realloc(*pfds, sizeof(**pfds) * (*fd_size));
-		// client_bufs
-		if (!*pfds) err ("Fatal error");
+// Connfd - socket. fd_count - total number of sockets
+void	add_client(int connfd) {
+	if (fd_count == fd_size) {
+		if (fd_size == 0) {
+			fd_size = 5;
+		} else {
+			fd_size = fd_size * 2;
+		}
+		pfds = realloc(pfds, sizeof(*pfds) * (fd_size));
+		client_bufs = realloc(client_bufs, sizeof(*client_bufs) * (fd_size));
+		if (!pfds || !client_bufs) err ("Fatal error");
+
 	}
-
-	int indx = *fd_count;
-	(*pfds)[indx].fd = connfd;
-	(*pfds)[indx].events = POLLIN;
-	// revents ?
-
-	(*fd_count)++; // count connections;
+	pfds[fd_count].fd = connfd;
+	pfds[fd_count].events = POLLIN;
+	// TO DO
+	pfds[fd_count].revents = 0;
+	client_bufs[fd_count] = NULL; // Ініціалізуємо буфер як порожній
+	fd_count++;
 }
 
-void	handle_new_conn(
-	int sockfd, int *fd_count, int *fd_size, struct pollfd **pfds) {
+// повертає 1, якщо видалив
+int	handle_client_data(int pfd_i) {
+	char recv_buf[1024]; // Буфер для одного виклику recv
+	int sender_fd = pfds[pfd_i].fd;
 
+	int nbytes = recv(sender_fd, recv_buf, sizeof(recv_buf), 0);
+	if (nbytes <= 0) { // Клієнт від'єднався або сталася помилка
+		char msg_buf[100];
+        sprintf(msg_buf, "server: client %d just left\n", client_ids[sender_fd]);
+		remove_client(pfd_i);
+		broadcast_msg(sender_fd, msg_buf); // Повідомляємо всіх, що клієнт пішов
+		return 1;
+	} else {
+        recv_buf[nbytes] = '\0';
+        // 1. Додаємо отримані дані до персонального буфера клієнта
+        client_bufs[pfd_i] = str_join(client_bufs[pfd_i], recv_buf);
+
+		// 2. Обробляємо всі повні повідомлення в буфері
+        char *msg = NULL;
+		while (extract_message(&client_bufs[pfd_i], &msg)) {
+            // 3. Формуємо фінальне повідомлення з префіксом
+            char broadcast_buf[4200]; // Розмір має бути достатнім
+            sprintf(broadcast_buf, "client %d: %s", client_ids[sender_fd], msg);
+			// 4. Розсилаємо його всім іншим
+        	broadcast_msg(sender_fd, broadcast_buf);
+			// 5. Звільняємо пам'ять, виділену extract_message
+            free(msg);
+		}
+		return 0;
+	}
+	return 0;
+}
+
+void	handle_new_conn() {
 	struct sockaddr_in	cli;
 	socklen_t			len = sizeof(cli);
-	int					connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-	if (connfd == -1) err("Fatal error");
-	add_to_pfds(connfd, fd_count, fd_size, pfds);
+	int					connfd;
+	connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+	if (connfd == -1) err("Fatal error"); // TO DO: ? return; Не фатальна помилка, просто ігноруємо
+	
+	add_client(connfd);
+	client_ids[connfd] = next_id;
+	next_id++;
 
-	char msg_buf[210000];
-	sprintf(msg_buf, "server: client %d just arrived\n", connfd);
-
-	for (int i = 0; i < *fd_count; i++) {
-		if ((*pfds)[i].fd != sockfd) {
-			if (send((*pfds)[i].fd, msg_buf, strlen(msg_buf), 0) == -1) err ("Fatal error");
-		}
-	}
-}
-
-void	process_connections(
-	int sockfd, int *fd_count, int *fd_size, struct pollfd **pfds) {
-		for (int i = 0; i < *fd_count; i++) {
-			if ((*pfds)[i].revents != 0) {
-				printf("  fd=%d; events: %s%s%s\n", (*pfds)[i].fd,
-                    ((*pfds)[i].revents & POLLIN)  ? "POLLIN "  : "",
-                    ((*pfds)[i].revents & POLLHUP) ? "POLLHUP " : "",
-                    ((*pfds)[i].revents & POLLERR) ? "POLLERR " : "");
-			}
-			if ((*pfds)[i].fd == sockfd) {
-				handle_new_conn(sockfd, fd_count, fd_size, pfds);
-				// to all: "server: client %d just arrived\n"
-			} else {
-				handle_client_data(sockfd, i, fd_count, pfds);
-			}
-		}
+	char	msg_buf[100];
+	sprintf(msg_buf, "server: client %d just arrived\n", client_ids[connfd]);
+	broadcast_msg(connfd, msg_buf);
 }
 
 int	main(int argc, char **argv) {
 	if (argc < 2) err("Wrong number of arguments");
 
 	int	port = atoi(argv[1]);
-	int	sockfd;
 
 	sockfd = get_listener_socket(port);	
 	if (sockfd == -1) err("Fatal error");
 	
-	struct pollfd	*pfds;
-	int				fd_size = 5; // realloc as necessary
-	pfds = calloc(fd_size, sizeof(*pfds) * fd_size);
-	if (pfds == NULL) err("Fatal error");
+	add_client(sockfd);
 
-	pfds[0].fd = sockfd;
-	pfds[0].events = POLLIN;
-	int fd_count = 1; // tracks the number of structs in use
-
-	while(1)	{
-		int	ready;
-		ready = poll(pfds, fd_count, -1);
+	while(1) {
+		int	ready = poll(pfds, fd_count, -1);
 		if (ready == -1) err("Fatal error");
-
-		process_connections(sockfd, &fd_count, &fd_size, &pfds);
+		for (int i = 0; i < fd_count; i++) {
+			if (pfds[i].revents & POLLIN) {
+				if (pfds[i].fd == sockfd) 
+					handle_new_conn();
+				else
+					handle_client_data(i);
+			}
+		}
 	}
 	close(sockfd);
-	free (pfds);
+	free(pfds);
 	free(client_bufs);
     return 0;
 }
